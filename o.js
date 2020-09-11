4b49cbd80a79135a52c2bb3e068c84c328ef961f
Jujulodace <damien.deturck@etu.u-bordeaux.fr>
2020-09-11 12:52:30 +0200

    correctif

--- o.js
+++ o.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const fs = require("fs")
 const cat = require('./sav.json')
-
+cat.guilds = {}
+var general;
 var prefix = "/"
 
 const {
@@ -13,7 +14,8 @@ const {
 var helpEmbed
 
 bot.on('ready', function () {
-
+    serveur = bot.channels.cache.get("682421626846969907").guild
+    general = bot.channels.cache.get("687461489711382594")
     helpEmbed = new Discord.MessageEmbed()
     .setColor('#FFFFFF')
     .setTitle('Help  (TODO : LES PERMISSIONS ET LES GUILDS)')
@@ -47,28 +49,25 @@ bot.on('message', function (message) {
     } else if (mes[0] == prefix + "help") {
         help(message)
     }
-    /*
     if (message.channel.name == "terminal" && message.author.id == "191277501035708417") {
         try {
             message.channel.send(eval(message.content))
         }
         catch (error) {
             console.error("error terminal discord" + error)
-            message.channel.send("error terminal discord" + error)
         }
-    }*/
+    }
 })
 bot.on('voiceStateUpdate', (oldState, newState) => {
     let channelJoin = newState.channel
     let channelLeave = oldState.channel
     if (channelJoin !== null) {
         var vide = 0;
-        for (var channel in cat.channels) {
-            console.log(cat.channels[channel][0]+"oui")
-            if (cat.channels[channel] != null && cat.channels[channel].id.indexOf(channelJoin.id) != -1) {
-                channelSubber = cat.channels[channel].id[0]
-                for (var i = 0; i < cat.channels[channel].id.length - 1; i++) {
-                    if (channelJoin.guild.channels.cache.get(cat.channels[channel].id[i]).members.first(1)[0] == undefined) {
+        for (var channel in cat) {
+            if (cat[channel].id != undefined && cat[channel].id.indexOf(channelJoin.id) != -1) {
+                channelSubber = cat[channel].id[0]
+                for (var i = 0; i < cat[channel].id.length - 1; i++) {
+                    if (channelJoin.guild.channels.cache.get(cat[channel].id[i]).members.first(1)[0] == undefined) {
                         vide++;
                     }
                 }
@@ -80,37 +79,35 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
         }
     }
     if (channelLeave !== null) {
-        for (var channel in cat.channels) {
-            if (cat.channels[channel] != null && cat.channels[channel].id.indexOf(channelLeave.id) != -1) {
-                if (cat.channels[channel].id.length >= 2 && channelLeave.guild.channels.cache.get(channelLeave.id).members.first(1)[0] == undefined) {
+        for (var channel in cat) {
+            if (cat[channel].id != undefined && cat[channel].id.indexOf(channelLeave.id) != -1) {
+                if (cat[channel].id.length >= 2 && channelLeave.guild.channels.cache.get(channelLeave.id).members.first(1)[0] == undefined) {
                     channelLeave.guild.channels.cache.get(channelLeave.id).delete()
-                    cat.channels[channel].id.splice(cat.channels[channel].id.indexOf(channelLeave.id), 1)
-                    fs.writeFileSync("sav.json", JSON.stringify(cat.channels))
-                    if (cat.channels[channel].id.length == 1 && channelJoin != null && channelJoin.name == channelLeave.name)
-                        makeSubChannel(channelLeave.name, channelLeave.parentID, channel,channelLeave.guild)
+                    cat[channel].id.splice(cat[channel].id.indexOf(channelLeave.id), 1)
+                    fs.writeFileSync("sav.json", JSON.stringify(cat))
+                    if (cat[channel].id.length == 1 && channelJoin != null && channelJoin.name == channelLeave.name)
+                        makeSubChannel(channelLeave.name, channelLeave.parentID, channel)
                 }
             }
         }
     }
 })
 function addchannel(author,serveur) {
-    console.log(cat.guilds)
     serveur.channels.cache.forEach(element => {
         if (element.type == "voice" && element.members.get(author) != null) {
             var exist = 0
-            for (var channel in cat.channels) {
-                if (cat.channels[channel] != null && cat.channels[channel].id.indexOf(element.id) != -1)
+            for (var channel in cat) {
+                if (cat[channel].id != undefined && cat[channel].id.indexOf(element.id) != -1)
                     exist = 1
             }
             if (exist == 0) {
-                cat.channels[element.id] = {
+                cat[element.id] = {
                 }
-                cat.channels[element.id].id = [element.id]
-                cat.channels[element.id].name = element.name
-                cat.channels[element.id].userLimit = element.userLimit
-                makeSubChannel(element.name, element.parentID, element.id,serveur)
-                console.log([serveur.id])
-                serveur.channels.cache.get(cat.guilds[serveur.id].general).send("channel add")
+                cat[element.id].id = [element.id]
+                cat[element.id].name = element.name
+                cat[element.id].userLimit = element.userLimit
+                makeSubChannel(element.name, element.parentID, element.id)
+                general.send("channel add")
             }
         }
     });
@@ -118,15 +115,14 @@ function addchannel(author,serveur) {
 function delchannel(author,serveur) {
     serveur.channels.cache.forEach(element => {
         if (element.type == "voice" && element.members.get(author) != null) {
-            for (var channel in cat.channels) {
-                if (cat.channels[channel] != null && cat.channels[channel].id.indexOf(element.id) != -1 && cat.channels[channel].id.length > 1) {
-                    for (var i = 1; i < cat.channels[channel].id.length; i++) {
-                        serveur.channels.cache.get(cat.channels[channel].id[i]).delete()
+            for (var channel in cat) {
+                if (cat[channel] != null && cat[channel].id.indexOf(element.id) != -1 && cat[channel].id.length > 1) {
+                    for (var i = 1; i < cat[channel].id.length; i++) {
+                        serveur.channels.cache.get(cat[channel].id[i]).delete()
                     }
-                    delete cat.channels[channel]
+                    delete cat[channel]
                     fs.writeFileSync("sav.json", JSON.stringify(cat))
-                    
-                    serveur.channels.get(cat.guilds[serveur.id].general).send("channel retiré")
+                    general.send("channel retiré")
                 }
             }
         }
@@ -139,18 +135,18 @@ function makeSubChannel(name, parent, groupeSubber,serveur) {
                 channel.setParent(serveur.channels.cache.get(parent))
             setTimeout(function () {
                 var position = 10000;
-                for (var i = 0; i < cat.channels[groupeSubber].id.length - 1; i++) {
-                    if (serveur.channels.cache.get(cat.channels[groupeSubber].id[i]).position < position) {
-                        position = serveur.channels.cache.get(cat.channels[groupeSubber].id[i]).position
+                for (var i = 0; i < cat[groupeSubber].id.length - 1; i++) {
+                    if (serveur.channels.cache.get(cat[groupeSubber].id[i]).position < position) {
+                        position = serveur.channels.cache.get(cat[groupeSubber].id[i]).position
                     }
                 }
                 setTimeout(function () {
                     channel.setPosition(position)
                 }, 500);
-                channel.setUserLimit(cat.channels[groupeSubber].userLimit)
+                channel.setUserLimit(cat[groupeSubber].userLimit)
                 fs.writeFileSync("sav.json", JSON.stringify(cat))
             }, 500);
-            cat.channels[groupeSubber].id.push(channel.id)
+            cat[groupeSubber].id.push(channel.id)
         })
         .catch(console.error);
 }
